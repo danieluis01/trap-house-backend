f15243077a3f6791df256b13adf03ece67466bbc
danieluis01 <danielluisteia@gmail.com>
2025-05-03 01:37:01 +0100

    Update music.js

--- backend/src/routes/music.js
+++ backend/src/routes/music.js
@@ -1,1 +1,21 @@
+// Adicione aqui as rotas de músicaconst express = require('express');
+const router = express.Router();
+
 // Adicione aqui as rotas de música
+router.get('/', (req, res) => {
+  res.send('Listando todas as músicas');
+});
+
+router.post('/', (req, res) => {
+  res.send('Criando uma nova música');
+});
+
+router.put('/:id', (req, res) => {
+  res.send(`Atualizando música com ID ${req.params.id}`);
+});
+
+router.delete('/:id', (req, res) => {
+  res.send(`Deletando música com ID ${req.params.id}`);
+});
+
+module.exports = router;
